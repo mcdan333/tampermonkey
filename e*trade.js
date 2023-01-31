17e0186aa28aac200999251fe6e4339ef1a813e7
mcdan333 <61846627+mcdan333@users.noreply.github.com>
2023-01-31 11:11:19 -0800

    Create e*trade.js

---
+++ e*trade.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         E*Trade UI tweaks
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://us.etrade.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=etrade.com
+// @grant        none
+// ==/UserScript==
+
+/* eslint-env jquery */
+/* eslint no-multi-spaces:0 */
+
+(function() {
+    'use strict';
+
+    //$("#security-code-chk").prop("checked", true);
+    $("#security-code-chk").trigger('click');
+
+})();
